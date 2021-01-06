1498050b14d561f4084e6a815c7929ed456420d1
Rebecca88 <rebecca88oliver@gmail.com>
2021-01-06 18:48:09 -0500

    Updates

---
+++ precache-manifest.ab8086e43831a3b30ba21b76c261d03b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43e94cd2f6f10e2f923e5cbd56c95fd3",
+    "url": "/Employee-Directory-with-React-JS/index.html"
+  },
+  {
+    "revision": "210aa8589e984d3583c1",
+    "url": "/Employee-Directory-with-React-JS/static/css/main.0212e535.chunk.css"
+  },
+  {
+    "revision": "b965933917425086dd41",
+    "url": "/Employee-Directory-with-React-JS/static/js/2.8809fcf8.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Employee-Directory-with-React-JS/static/js/2.8809fcf8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "210aa8589e984d3583c1",
+    "url": "/Employee-Directory-with-React-JS/static/js/main.08b12686.chunk.js"
+  },
+  {
+    "revision": "879274d8b32c75e903ae",
+    "url": "/Employee-Directory-with-React-JS/static/js/runtime-main.b6a8d5a0.js"
+  }
+]);
